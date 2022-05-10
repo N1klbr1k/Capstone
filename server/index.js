8182230291137fd67b186cb08a8f3ebddfc3c775
N1klbr1k <lemmonynikit@gmail.com>
2022-05-10 11:06:27 -0600

    implemented changes to index.js

--- server/index.js
+++ server/index.js
@@ -3,23 +3,22 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const path = require('path')
-const {client } = require('pg')
 
-const { SERVER_PORT } = process.env;
+
+//const { SERVER_PORT } = process.env;
 const { seed, getCreature, createCreature,getAllCreatures,getAttack } = require("./controller.js");
 
 
 
 app.use(express.json());
 app.use(cors());
-const port = process.env.PORT || 4545
 
 
 //----middleware -------
-  
-   app.use(express.static(path.join(__dirname, '../public')));
 
-    
+app.use(express.static(path.join(__dirname, '../public')));
+
+
 //seed the database
 app.post("/seed", seed);
 
@@ -33,5 +32,6 @@ app.get('/creatures',getAllCreatures);
 
 app.post("/creatures", createCreature);
 
+const port = process.env.PORT || 4545
 
-app.listen(SERVER_PORT, () => console.log(`listening on port ${SERVER_PORT}`));
+app.listen(port, () => console.log(`listening on port ${SERVER_PORT}`));
