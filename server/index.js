7c657de4509c5f3186d9627095584f83552afd03
N1klbr1k <lemmonynikit@gmail.com>
2022-05-11 17:56:40 -0600

    trying to get stuff to work

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cors());
 
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static('node_modules'))
+//app.use(express.static('node_modules'))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
