7a4859b352deddafb3d1f44e1a17cdfa9350de93
N1klbr1k <lemmonynikit@gmail.com>
2022-05-11 18:04:40 -0600

    something changed

--- server/index.js
+++ server/index.js
@@ -17,9 +17,9 @@ app.use(cors());
 //----middleware -------
 
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
 
 app.use(express.static(path.join(__dirname, '../public')));
