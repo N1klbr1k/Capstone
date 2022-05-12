ccc270423717aa43d4f937a3b0c5f732f515381d
N1klbr1k <lemmonynikit@gmail.com>
2022-05-11 18:03:30 -0600

    something

--- server/index.js
+++ server/index.js
@@ -17,14 +17,14 @@ app.use(cors());
 //----middleware -------
 
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 
 app.use(express.static(path.join(__dirname, '../public')));
 //app.use(express.static('node_modules'))
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 //seed the database
 app.post("/seed", seed);
 
