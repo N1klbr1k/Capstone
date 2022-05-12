d6e33a94c18457bbcd33c8e280fef317a62e85c3
N1klbr1k <lemmonynikit@gmail.com>
2022-05-11 18:07:46 -0600

    trying to get app.get to server root

--- server/index.js
+++ server/index.js
@@ -21,6 +21,10 @@ app.use(cors());
 //     res.sendFile(path.join(__dirname, "../public/index.html"));
 // });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
 
 app.use(express.static(path.join(__dirname, '../public')));
 //app.use(express.static('node_modules'))
